2c2fbf101dbbb6236223662e2bb515814bb94fe8
JaponBaligi <f.123lee123@gmail.com>
2025-02-01 00:00:43 +0300

    test

--- today.py
+++ today.py
@@ -220,44 +220,61 @@ def cache_builder(edges, comment_size, force_cache, loc_add=0, loc_del=0):
     Checks each repository in edges to see if it has been updated since the last time it was cached
     If it has, run recursive_loc on that repository to update the LOC count
     """
-    cached = True # Assume all repositories are cached
-    filename = 'cache/'+hashlib.sha256(USER_NAME.encode('utf-8')).hexdigest()+'.txt' # Create a unique filename for each user
+    cached = True  # Assume all repositories are cached
+    filename = 'cache/' + hashlib.sha256(USER_NAME.encode('utf-8')).hexdigest() + '.txt'  # Create a unique filename for each user
+
     try:
         with open(filename, 'r') as f:
             data = f.readlines()
-    except FileNotFoundError: # If the cache file doesn't exist, create it
+    except FileNotFoundError:  # If the cache file doesn't exist, create it
         data = []
         if comment_size > 0:
             for _ in range(comment_size): data.append('This line is a comment block. Write whatever you want here.\n')
         with open(filename, 'w') as f:
             f.writelines(data)
 
-    if len(data)-comment_size != len(edges) or force_cache: # If the number of repos has changed, or force_cache is True
+    # If the number of repos has changed, or force_cache is True
+    if len(data) - comment_size != len(edges) or force_cache:
         cached = False
         flush_cache(edges, filename, comment_size)
         with open(filename, 'r') as f:
             data = f.readlines()
 
-    cache_comment = data[:comment_size] # save the comment block
-    data = data[comment_size:] # remove those lines
+    cache_comment = data[:comment_size]  # Save the comment block
+    data = data[comment_size:]  # Remove those lines
+
+    # Debugging: Print the length of data and edges to understand the mismatch
+    print(f"Number of edges: {len(edges)}")
+    print(f"Number of data lines: {len(data)}")
+
     for index in range(len(edges)):
+        if index >= len(data):
+            print(f"Warning: Not enough data lines for index {index}. Skipping this entry.")
+            continue  # Skip processing if there are not enough data lines
+
         repo_hash, commit_count, *__ = data[index].split()
+
         if repo_hash == hashlib.sha256(edges[index]['node']['nameWithOwner'].encode('utf-8')).hexdigest():
             try:
                 if int(commit_count) != edges[index]['node']['defaultBranchRef']['target']['history']['totalCount']:
-                    # if commit count has changed, update loc for that repo
+                    # If commit count has changed, update loc for that repo
                     owner, repo_name = edges[index]['node']['nameWithOwner'].split('/')
                     loc = recursive_loc(owner, repo_name, data, cache_comment)
                     data[index] = repo_hash + ' ' + str(edges[index]['node']['defaultBranchRef']['target']['history']['totalCount']) + ' ' + str(loc[2]) + ' ' + str(loc[0]) + ' ' + str(loc[1]) + '\n'
-            except TypeError: # If the repo is empty
+            except TypeError:  # If the repo is empty
                 data[index] = repo_hash + ' 0 0 0 0\n'
+
+    # Write the updated data back to the file
     with open(filename, 'w') as f:
         f.writelines(cache_comment)
         f.writelines(data)
+
+    # Calculate the total LOC added and deleted
     for line in data:
         loc = line.split()
         loc_add += int(loc[3])
         loc_del += int(loc[4])
+
     return [loc_add, loc_del, loc_add - loc_del, cached]
 
 
