df1998a3ea3cac4ea1ffdeac6da413d141b85654
JaponBaligi <f.123lee123@gmail.com>
2026-03-14 09:47:55 +0300

    should not touch ig

--- today.py
+++ today.py
@@ -371,7 +371,7 @@ def svg_overwrite(filename, age_data, commit_data, star_data, repo_data, contrib
     justify_format(root, 'contrib_data', contrib_data)
     justify_format(root, 'follower_data', follower_data, 10)
     justify_format(root, 'loc_data', loc_data[2], 9)
-    justify_format(root, 'loc_add', loc_data[0])
+    justify_format(root, 'loc_add', loc_data[0], 7)
     justify_format(root, 'loc_del', loc_data[1], 7)
     justify_format(root, 'age_data', age_data)
     tree.write(filename, encoding='utf-8', xml_declaration=True)
